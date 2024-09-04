45e0124758e765329b56927ab08286ae05d49e18
Timothy itayi <timmytam10@gmail.com>
2024-09-04 19:58:18 +1000

    add new styling to buttons

---
+++ public/js/main.js
@@ -0,0 +1,18 @@
+window.onload = function() {
+    const container = document.getElementById('animatedTextContainer');
+    const animationTiming = parseInt(container.getAttribute('data-animation-timing'), 10) || 3000;
+    const animatedTexts = document.querySelectorAll('.text-slide-up-fade-blur');
+    if (!animatedTexts.length) return;
+    animatedTexts.forEach(text => {
+    text.style.animation = `textSlideUpFadeBlur ${animationTiming}ms ease-in-out forwards`;
+    });
+    let currentText = 0;
+    
+    function animateText() {
+    animatedTexts.forEach(text => (text.style.display = 'none'));
+    animatedTexts[currentText].style.display = 'inline';
+    currentText = (currentText + 1) % animatedTexts.length;
+    }
+    animateText();
+    setInterval(animateText, animationTiming);
+    };
