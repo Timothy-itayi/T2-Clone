5bd4fa5871308a163cf134e648d5f38c5aa1566d
Timothy itayi <timmytam10@gmail.com>
2024-09-10 19:35:39 +1000

    update card to product detail and products update links

--- server.js
+++ server.js
@@ -6,20 +6,15 @@ const PORT = process.env.PORT || 3000;
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/files', express.static(path.join(__dirname, 'files')));
+
 
 // Home Route
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// Products Route
-app.get('/products', (req, res) => {
-    res.sendFile(path.join(__dirname, 'products.html'));
-});
 
-// Product Detail Route
-app.get('/product-detail', (req, res) => {
-    res.sendFile(path.join(__dirname, 'product-detail.html'));
-});
+
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
