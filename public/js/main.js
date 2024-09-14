84f7832422c3ccd2b63c8f82926a2bfd3d724909
Timothy itayi <timmytam10@gmail.com>
2024-09-14 16:32:22 +1000

    update nav to have side nav

--- public/js/main.js
+++ public/js/main.js
@@ -18,13 +18,21 @@
 //     };
 
 
-var burgerMenu = document.getElementById('burger-menu');
+// var burgerMenu = document.getElementById('burger-menu');
 
-var overlay = document.getElementById('menu-list');
+// var overlay = document.getElementById('menu-list');
 
-burgerMenu.addEventListener('click', function() {
-  this.classList.toggle("close");
-  overlay.classList.toggle("overlay");
-});
+// burgerMenu.addEventListener('click', function() {
+//   this.classList.toggle("close");
+//   overlay.classList.toggle("overlay");
+// });
 
+/* Set the width of the side navigation to 250px */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
 
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
