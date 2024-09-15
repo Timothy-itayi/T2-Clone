b3f0c0299e61dd18d8e569bc915a64d76b5fe330
Timothy itayi <timmytam10@gmail.com>
2024-09-15 22:03:12 +1000

    add the image to card detail

--- public/js/main.js
+++ public/js/main.js
@@ -64,3 +64,63 @@ buttons.forEach(button => {
     }
   });
 });
+
+
+
+gsap.registerPlugin(Observer);
+
+const gallerySection = document.querySelector(".gallery-section");
+const galleryCards = document.querySelectorAll(".gallery .cards li");
+const galleryImages = document.querySelectorAll(".gallery .product-image");
+gsap.set(outerWrappers, { yPercent: 100 });
+gsap.set(innerWrappers, { yPercent: -100 });
+
+function gotoSection(index, direction) {
+  index = wrap(index); // make sure it's valid
+  animating = true;
+  let fromTop = direction === -1,
+      dFactor = fromTop ? -1 : 1,
+      tl = gsap.timeline({
+        defaults: { duration: 1.25, ease: "power1.inOut" },
+        onComplete: () => animating = false
+      });
+  if (currentIndex >= 0) {
+    // The first time this function runs, current is -1
+    gsap.set(sections[currentIndex], { zIndex: 0 });
+    tl.to(images[currentIndex], { yPercent: -15 * dFactor })
+      .set(sections[currentIndex], { autoAlpha: 0 });
+  }
+  gsap.set(sections[index], { autoAlpha: 1, zIndex: 1 });
+  tl.fromTo([outerWrappers[index], innerWrappers[index]], { 
+      yPercent: i => i ? -100 * dFactor : 100 * dFactor
+    }, { 
+      yPercent: 0 
+    }, 0)
+    .fromTo(images[index], { yPercent: 15 * dFactor }, { yPercent: 0 }, 0)
+    .fromTo(splitHeadings[index].chars, { 
+        autoAlpha: 0, 
+        yPercent: 150 * dFactor
+    }, {
+        autoAlpha: 1,
+        yPercent: 0,
+        duration: 1,
+        ease: "power2",
+        stagger: {
+          each: 0.02,
+          from: "random"
+        }
+      }, 0.2);
+
+  currentIndex = index;
+}
+
+Observer.create({
+  type: "wheel,touch,pointer",
+  wheelSpeed: -1,
+  onDown: () => !animating && gotoSection(currentIndex - 1, -1),
+  onUp: () => !animating && gotoSection(currentIndex + 1, 1),
+  tolerance: 10,
+  preventDefault: true
+});
+
+gotoSection(0, 1);
