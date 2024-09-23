3b7a03c042bcbe61c65283bf6ea295c1544e428d
Timothy itayi <timmytam10@gmail.com>
2024-09-23 19:12:52 +1000

    update netlify config

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -4,11 +4,15 @@ const path = require('path');
 
 const app = express();
 
-// Static files middleware
+// Serve static files from the 'public' folder
 app.use(express.static(path.join(__dirname, '../public')));
 
+// Serve files from 'files' folder if necessary
+app.use('/files', express.static(path.join(__dirname, '../files')));
+
+// Serve the index.html from the root directory
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/index.html'));
+  res.sendFile(path.join(__dirname, '../index.html'));
 });
 
 module.exports.handler = serverless(app);
